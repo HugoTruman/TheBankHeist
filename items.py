89a23ef2a84c97065d81980a8ed5654180ad1669
HugoTruman <trumanh@cardiff.ac.uk>
2018-10-22 15:26:00 +0100

    1st virson

---
+++ items.py
@@ -0,0 +1,47 @@
+item_id = {
+    "id": "id",
+
+    "name": "id card",
+
+    "description":
+        """This card contains the details of one of the bank manages. Jing Wu. it has clearence to the break room""",
+
+    "mass": 0.1,
+}
+
+item_password = {
+    "id": "password",
+
+    "name": "password",
+
+    "description":
+        "This sheet of paper contains the password for the security computers.",
+    "mass": 2.5,
+}
+
+item_money = {
+    "id": "money",
+
+    "name": "money",
+
+    "description":
+        "This mountain of cash is almost enough to pay off your student debt.",
+    "mass": 0.3,
+}
+
+item_tape = {
+    "id": "tape",
+
+    "name": "tape",
+
+    "description":
+        "An empty tape taken from the security system.",
+    "mass": 0.3,
+}
+
+items = {
+    "id": item_id,
+    "password": item_password,
+    "money": item_money,
+    "tape": item_tape,
+}
