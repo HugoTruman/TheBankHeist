cd2c07e2306d6956453f34b651da5448221a7133
EthanKerai <ethankerai@gmail.com>
2018-10-22 16:27:32 +0100

    Finished Functions of parser

--- gameParser.py
+++ gameParser.py
@@ -1,16 +1,34 @@
 import string
 
 #Define a list of words not to remove from input
-important_words = ["",""] # TODO
-
-
+important_words = []
 
 def filter_words(words):
 	#This function removes the unimportant words from a string
+
+	useful_words = []
 	for item in words:
-		if item not in important_words:
-			words.remove(item)
-			continue
-		else:
-			pass
-	return words
+		if item in important_words:
+			useful_words.append(item)
+	return useful_words
+
+
+def remove_punct(text):
+	#This function removes punctuation from a given string
+	no_punct = ""
+
+	for char in text:
+		if not (char in string.punctuation):
+			no_punct += char
+	return no_punct
+
+def normalise_input(user_input):
+	#Returns a list of strings which are suitable to be understood by the game
+	no_punct = remove_punct(user_input).lower()
+	split_input = no_punct.split()
+
+	filtered = filter_words(split_input)
+	return filtered
+
+#print(normalise_input("I want to GO towards the south Exit!!!!")) TEST for normalise input func
+#test means other functions work also
