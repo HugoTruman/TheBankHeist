934655e54b748cf05cfeeefc1c2304f74d9d0853
HugoTruman <44092254+HugoTruman@users.noreply.github.com>
2018-10-22 16:59:38 +0100

    Added more store item

--- items.py
+++ items.py
@@ -27,6 +27,7 @@ item_money = {
     "description":
         "This mountain of cash is almost enough to pay off your student debt.",
     "mass": 0.3,
+
 }
 
 item_tape = {
@@ -39,9 +40,148 @@ item_tape = {
     "mass": 0.3,
 }
 
+item_pistol = {
+    "id": "pistol",
+
+    "name": "pistol",
+
+    "description":
+        "A fairly discrete weapon. Good for controlling small crowds or people ",
+    "mass":1.0,
+    "value":20,
+    "discrete":20
+}
+
+item_shotgun = {
+    "id": "shotgun",
+
+    "name": "shotgun",
+
+    "description":
+        "A loud alternative to other weapons. works even better for controlling crowds.",
+
+    "mass": 3.5,
+    "value":40,
+    "discrete":40
+}
+
+item_grenade = {
+    "id": "grenade",
+
+    "name": "grenade",
+
+    "description":
+        "Probaly not the best thing to take to a bank heist, but would be cool if you "
+        "could pull it off.",
+    "mass":0.5,
+    "value":15,
+    "discrete":50
+}
+
+item_smoke = {
+    "id": "smoke bomb",
+
+    "name": "smoke bomb",
+
+    "description":
+        "Good for hiding your self from police or bank tellers so they don't know "
+        "whats going on. Good for reducing the chances of calling the police.",
+    "mass":0.6,
+    "value":10,
+    "discrete":-10
+}
+
+item_smg = {
+    "id": "smg",
+
+    "name": "smg",
+
+    "description":
+        "Very good for controling a crowd but does have a fair amount of noise.",
+    "mass":4,
+    "value":35,
+    "discrete":35
+}
+
+item_rpg= {
+    "id": "rpg",
+
+    "name": "rpg",
+
+    "description":
+        "This is very loud. You will definitely strike fear into your hostages.",
+    "mass":5,
+    "value":50,
+    "discrete":60
+}
+
+item_pigmask = {
+    "id": "pig mask",
+
+    "name": "pig mask",
+
+    "description":
+        "This helps hide your identity while also making it look like a pig is"
+        "robbing the back.",
+    "mass":0.5,
+    "value":7,
+    "discrete":-25
+}
+
+item_kirillmask = {
+    "id": "kirill mask",
+
+    "name": "kirill mask",
+
+    "description":
+        "this helps hiding your identity while also making it look like the main "
+        "reason why you are robbing the bank, student debt.",
+    "mass":.5,
+    "value":15,
+    "discrete"-40:
+}
+
+item_clownmask = {
+    "id": "clown mask",
+
+    "name": "clown mask",
+
+    "description":
+        "This helps hiding your identity while also making you look like a clown "
+        "to help scare anyone with a fear of clowns.",
+    "mass":0.5,
+    "value":7,
+    "discrete":-20
+}
+
+item_boilersuit = {
+    "id": "boiler suit",
+
+    "name": "boiler suit",
+
+    "description":
+        "used to hide your normal cloths so no one can relate back to you.",
+    "mass":1,
+    "value":15,
+    "discrete":-10
+}
+
 items = {
     "id": item_id,
     "password": item_password,
     "money": item_money,
     "tape": item_tape,
+    "pistol": item_pistol,
+    "shotgun": item_shotgun,
+    "grenade": item_grenade,
+    "smoke bomb": item_smoke,
+    "smg": item_smg,
+    "rpg": item_rpg,
+    "pig mask": item_pigmask,
+    "kirill mask":item_kirillmask,
+    "clown mask":item_clownmask,
+    "boiler suit":item_boilersuit,
+
+
+
 }
