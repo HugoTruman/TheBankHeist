bc77a520a5cf1b14cf2e5d7f7cbf404ffb217e96
Pigu1999 <johnsd2@cardiff.ac.uk>
2018-10-24 19:16:19 +0100

    Updated ending text

--- Endings.py
+++ Endings.py
@@ -1,24 +1,25 @@
 end_shot={
     "name":"Shot",
-    "description":"You have been shot! You are unable to continue. The police "
-                  "have been taken into custody."
+    "description":'''The police storm in and shout you cause you have a firearm. Also you were making far too much noise,
+    and quite frankly making a scene. Typical Americans.
+    You are unable to continue. The police have taken you into custody.'''
 }
 
 end_caught={
     "name": "Caught",
-    "description": "You have been caught! You are unable to continue. The police "
-                   "have been taken into custody."
+    "description": '''The police storm in, you were making far too much noise,
+    and quite frankly making a scene. Typical Americans
+    You are unable to continue. The police have taken you into custody.'''
 }
 
 end_get_away_cash={
     "name": "Cash Get Away",
-    "description": "You have been shot! You are unable to continue. The police "
-                   "Well done! You have gotten away. You can now pay back some of "
+    "description": "Well done! You have gotten away. You can now pay back some of "
                    "your student land"
 }
 
 end_arrested={
-    "name": "Get Away",
+    "name": "Arrested",
     "description": "You didn't get caught during the act."
                     "However you were far to noisy and obvious that the police worked out who you were."
                     "Unlucky you win nothing."
