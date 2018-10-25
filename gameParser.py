8dcac2c719776b068d216fd4580e9a8ecc1f3e3d
Pigu1999 <johnsd2@cardiff.ac.uk>
2018-10-25 13:23:23 +0100

    Added drop to game parser

--- gameParser.py
+++ gameParser.py
@@ -1,7 +1,7 @@
 import string
 
 #Define a list of words not to remove from input
-important_words = ["go","take","buy", "use",
+important_words = ["go","take","buy", "use", 'drop',
 				"north","east","south","west",
 				"rpg","id","password","money","tape","pistol","shotgun","grenade","smoke bomb", "smg", 'pig',
 				"kirill", "clown", "boiler", "ghost", "back", "sports",
